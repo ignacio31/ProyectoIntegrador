00c0aa5e0212845d327220bba75e60ddb26a7388
Ing Jorge Blancas <jblancas.es@gmail.com>
2019-01-19 12:03:52 -0600

    segunda modificacion

--- Main2.c
+++ Main2.c
@@ -1,5 +1,6 @@
 # include<stdio.h>
 
 int main(){
-	prinf("Hello world prueba de actualizacion ok breanch\n")
+   prinf("Hello world prueba de actualizacion ok breanch\n")
+   //segundo comentario
 }
